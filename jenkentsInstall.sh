99ce81e30830cc5bbd7bdb6e597ba0ba0618bf38
Sunil <sunil.nunubilli@gmail.com>
2024-12-15 20:51:21 +0530

    jenkins

---
+++ jenkentsInstall.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+
+#check whether root user or not
+R="\e[31m"
+N="\e[0m"
+
+sudo curl -o /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+# Add required dependencies for the jenkins package
+sudo yum install fontconfig java-17-openjdk
+sudo yum install jenkins
+sudo systemctl daemon-reload
